2f4db5a7b8e6da8bcdcca3698ff17ba90358235c
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-07-24 16:47:09 +0700

    +commit feature unlock-director

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
     User findByEmail(String email);
+
+    @Query(value = "SELECT * FROM user u join user_roles ur on u.id = ur.user_id where u.locked = 0 and ur.role_id = 2", nativeQuery = true)
+    public List<User> findDirectorLookedFalse ();
 }
